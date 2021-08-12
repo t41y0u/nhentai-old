45f422b100c3a8df4f3446a94e6f7d8365b89ade
t41y0u <44199462+t41y0u@users.noreply.github.com>
2021-08-12 20:53:09 +0700

    Update invite link #1

--- src/commands/info/invite.ts
+++ src/commands/info/invite.ts
@@ -15,9 +15,7 @@ export default class extends Command {
 
     async exec(message: Message) {
         const embed = this.client.embeds.default().setDescription(
-            `[Here](${await this.client.generateInvite({
-                permissions: PERMISSIONS,
-            })}) is my invite link!`
+            `[Here](https://discord.com/api/oauth2/authorize?client_id=663743798722953258&permissions=103116007424&scope=bot%20applications.commands) is my invite link!`
         );
         return message.channel.send({ embed });
     }
